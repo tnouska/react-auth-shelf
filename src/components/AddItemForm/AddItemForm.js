1ab9149854127d44fb4b77de7dd5a4c079d70792
Joshua John <JoshuaLeary@Danicas-MacBook-Pro.local>
2018-04-26 13:56:31 -0500

    Created Input form, Created handleInput(), Created local state(description, image_url)

--- src/components/AddItemForm/AddItemForm.js
+++ src/components/AddItemForm/AddItemForm.js
@@ -4,9 +4,38 @@ import Nav from '../../components/Nav/Nav';
 import { fetchUser } from '../../redux/actions/userActions';
 
 class AddItemForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            itemInputs: {
+                description: '',
+                image_url: ''
+            }
+        }
+    }
+
+    handleInput = (propertyName) => {
+        return (event) => {
+            console.log(event.target.value);            
+            this.setState({
+                itemInputs: {
+                    ...this.state.itemInputs,
+                    [propertyName]: event.target.value,
+                }
+            })
+        }
+    }
+
     render() {
         return (
-            <h1>Holla</h1>
+            <div>
+                <h1>Holla</h1>
+                <form>
+                    <input placeholder = "Description" type = "text" value = {this.state.description} onChange = {this.handleInput("description")} />
+                    <input placeholder = "Image URL" type = "text" value = {this.state.image_url}  onChange = {this.handleInput("image_url")}/>
+                    <button type = "submit">Submit</button>
+                </form>
+            </div>
         )
     }
 }
