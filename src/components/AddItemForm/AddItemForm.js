e2ce8fb6e285a129de298d4f485d47430c1f3f44
Joshua John <JoshuaLeary@Danicas-MacBook-Pro.local>
2018-04-26 13:42:14 -0500

    Setup component AddItemForm, Imported to App.js

---
+++ src/components/AddItemForm/AddItemForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Nav from '../../components/Nav/Nav';
+import { fetchUser } from '../../redux/actions/userActions';
+
+class AddItemForm extends Component {
+    render() {
+        return (
+            <h1>Holla</h1>
+        )
+    }
+}
+
+export default AddItemForm;
