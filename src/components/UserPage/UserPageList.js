bb9deeb921b860d6518b5dd246280e22a438a10b
Stephen Engler <sengler1989@gmail.com>
2018-04-27 09:37:15 -0500

    delete button works

--- src/components/UserPage/UserPageList.js
+++ src/components/UserPage/UserPageList.js
@@ -9,14 +9,20 @@ class UserPageList extends Component {
   constructor(props) {
     super(props)
   }
-
+  handleClick =()=>{
+    console.log('clicked ', this.props.view);
+    this.props.dispatch({
+      type: 'DELETE_ITEM',
+      payload: this.props.view
+    })
+  }
   render(){
 
     return(
       <div className="View">
         <h4>{this.props.view.description}</h4>
           <img src={this.props.view.image_url} />
-          <button>Delete</button>
+          <button onClick={this.handleClick}>Delete</button>
         </div>
     );
   }
